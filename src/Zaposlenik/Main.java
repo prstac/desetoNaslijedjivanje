260afc391472a53eed0cb4820fe2e507ad2a9b5d
josip <josip@laptop>
2025-02-17 20:23:24 +0100

    dodaj ispise prije poziva metoda

--- src/Zaposlenik/Main.java
+++ src/Zaposlenik/Main.java
@@ -8,13 +8,26 @@ public class Main {
         var zaposlenik = new Zaposlenik("Zaposlenik", 1);
         var programer = new Programer("Programer", 2);
         var manager = new Manager("Manager", 3);
+
+        System.out.println("Što radi programer?");
         programer.Radi();
+
+        System.out.println();
+
+        System.out.println("Što radi manager?");
         manager.Radi();
+
+        System.out.println();
+
         List<Zaposlenik> zaposlenici = new ArrayList<>();
         zaposlenici.add(zaposlenik);
         zaposlenici.add(manager);
         zaposlenici.add(programer);
+
+        System.out.println("Podaci o svim zaposlenicima:");
         zaposlenici.forEach(z -> z.getInfo());
+        System.out.println();
+        System.out.println("Svi rade:");
         zaposlenici.forEach(z -> z.Radi());
     }
 }
